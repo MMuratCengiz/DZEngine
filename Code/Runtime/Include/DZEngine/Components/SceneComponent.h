b28e1602c3de5ef133b12fa5816afb8854c10333
Murat Cengiz <cengizm9@gmail.com>
2025-08-02 14:36:07 +0200

    Move AssetHandle (Fix SceneComponent)

--- Code/Runtime/Include/DZEngine/Components/SceneComponent.h
+++ Code/Runtime/Include/DZEngine/Components/SceneComponent.h
@@ -19,7 +19,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #pragma once
 
 #include <string>
-#include "DZEngine/AssetHandle.h"
+#include "AssetHandle.h"
 
 namespace DZEngine
 {
